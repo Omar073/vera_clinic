8019e237706f07449a7f6e03e9e94655c4bdff89
Omar73 <120687551+Omar073@users.noreply.github.com>
2025-04-02 23:40:18 +0200

    revamped MFU UI

--- lib/MonthlyFollowUp/View/MonthlyFollowUp.dart
+++ lib/MonthlyFollowUp/View/MonthlyFollowUp.dart
@@ -46,88 +46,98 @@ class _MonthlyFollowUpState extends State<MonthlyFollowUp> {
           title: Text('${widget.client.mName!} :متابعة شهرية, عميل '),
           centerTitle: true,
         ),
-        body: Background(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                FutureBuilder<ClientMonthlyFollowUp?>(
-                    future: _fetchLastMonthlyFollowUp(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      if (snapshot.hasError) {
-                        return const Center(child: Text('حدث خطأ ما'));
-                      }
-                      if (snapshot.hasData) {
-                        final cmfu = snapshot.data!;
-                        return Column(
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 14.0),
-                              child: infoCard(
-                                "تفاصيل المتابعة الشهرية السابقة",
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                        child: infoField(
-                                            title: "BMI", value: "${cmfu.mBMI}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "PBF", value: "${cmfu.mPBF}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "BMR", value: "${cmfu.mBMR}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "Water",
-                                            value: "${cmfu.mWater}")),
-                                  ],
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Background(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  FutureBuilder<ClientMonthlyFollowUp?>(
+                      future: _fetchLastMonthlyFollowUp(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                        if (snapshot.hasError) {
+                          return const Center(child: Text('حدث خطأ ما'));
+                        }
+                        if (snapshot.hasData) {
+                          final cmfu = snapshot.data!;
+                          return Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 14.0),
+                                child: infoCard(
+                                  "تفاصيل المتابعة الشهرية السابقة",
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                          child: infoField(
+                                              title: "BMI",
+                                              value: "${cmfu.mBMI}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "PBF",
+                                              value: "${cmfu.mPBF}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "BMR",
+                                              value: "${cmfu.mBMR}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "Water",
+                                              value: "${cmfu.mWater}")),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 20),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 14.0),
-                              child: infoCard(
-                                "تفاصيل المتابعة الشهرية السابقة",
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                        child: infoField(
-                                            title: "Max weight",
-                                            value: "${cmfu.mMaxWeight}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "Optimal weight",
-                                            value: "${cmfu.mOptimalWeight}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "Max calories",
-                                            value: "${cmfu.mMaxCalories}")),
-                                    Expanded(
-                                        child: infoField(
-                                            title: "Daily calories",
-                                            value: "${cmfu.mDailyCalories}")),
-                                  ],
+                              const SizedBox(height: 20),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 14.0),
+                                child: infoCard(
+                                  "تفاصيل المتابعة الشهرية السابقة",
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                          child: infoField(
+                                              title: "Max weight",
+                                              value: "${cmfu.mMaxWeight}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "Optimal weight",
+                                              value: "${cmfu.mOptimalWeight}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "Max calories",
+                                              value: "${cmfu.mMaxCalories}")),
+                                      Expanded(
+                                          child: infoField(
+                                              title: "Daily calories",
+                                              value: "${cmfu.mDailyCalories}")),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
-                        );
-                      } else {
-                        return const Center(child: Text('لا توجد بيانات'));
-                      }
-                    }),
-                const SizedBox(height: 20),
-                newMonthlyFollowUpForm(),
-                const SizedBox(height: 20),
-                ActionButton(client: widget.client),
-              ],
+                              )
+                            ],
+                          );
+                        } else {
+                          return const Center(child: Text('لا توجد بيانات'));
+                        }
+                      }),
+                  const SizedBox(height: 20),
+                  newMonthlyFollowUpForm(),
+                  const SizedBox(height: 70),
+                  // Expanded(child: Container()),
+                  ActionButton(client: widget.client),
+                ],
+              ),
             ),
           ),
         ));
